a34f0d10b89fc2d446d5c7759299a8230346410b
Catherine Wang <catherinewangcca@gmail.com>
2019-03-04 11:41:01 -0800

    test

--- U9B-arraylist/ListOddToEven.java
+++ U9B-arraylist/ListOddToEven.java
@@ -3,13 +3,50 @@
 //Name -
 //Date -
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class ListOddToEven
-{
-  public static boolean go( List<Integer> ray )
-  {
-    return false;
+public class ListOddToEven{
+  ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+  int distance = -1;
+  int odd = -1;
+  int even = -1;
+
+public ListOddToEven(){
+}
+
+public ListOddToEven(ArrayList<Integer> ray){
+  setArray(ray);
+}
+
+public void setArray(ArrayList<Integer> ray){
+  numbers = ray;
+}
+
+public void oddEvenDistance(){
+  for (int i = 0; i<numbers.size(); i++){
+    if (numbers.get(i) % 2 == 1)
+    {
+      odd = i;
+      break;
+    }
   }
+  for (int x = numbers.size()-1; x>=0; x--){
+    if (numbers.get(x) % 2 == 0)
+    {
+      even = x;
+      break;
+    }
+  }  
+  if (odd != -1 && even != -1)
+  {
+    distance = even - odd;
+  }
+}
+
+
+public String toString(){
+  return distance + "";
+}
+
 }
--- U9B-arraylist/OddToEvenRunner.java
+++ U9B-arraylist/OddToEvenRunner.java
@@ -3,9 +3,14 @@
 //Name -
 //Date -
 
-public class OddToEvenRunner
-{
-  public static void main( String args[] )
-  {
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+  ArrayList<Integer> numList = new ArrayList<Integer>(Arrays.asList(32767,70,4,5,6,7)); 
+  ListOddToEven test = new ListOddToEven();
+  test.setArray(numList);
+  test.oddEvenDistance();
+  System.out.print(test.toString());
+
   }
 }
